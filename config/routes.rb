da82f1b5dfdd33aa6436d204735a643bb4caf5c9
Oğulcan Girginç <ogirginc@ogirginc.com>
2022-11-03 22:23:42 +0300

    Add basic authentication
    Use authentication-zero gem[1] to generate a pre-built authentication
    system that follows both security and Rails best practices. By
    generating code into the application instead of using a library, we have
    complete freedom to modify the authentication system so it works best
    for our use cases.
    
    [1] https://github.com/lazaronixon/authentication-zero

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  root 'home#index'
+
+  get  'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+  get  'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
+
+  resources :sessions, only: [:index, :show, :destroy]
+  resource  :password, only: [:edit, :update]
+  namespace :identity do
+    resource :email,              only: [:edit, :update]
+    resource :email_verification, only: [:edit, :create]
+    resource :password_reset,     only: [:new, :edit, :create, :update]
+  end
 end
