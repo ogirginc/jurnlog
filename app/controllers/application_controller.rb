a4ee2a0a77dde01dcddf078260615d810f1de57d
Oğulcan Girginç <ogirginc@ogirginc.com>
2022-11-21 21:50:27 +0300

    Add basic rich text support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,8 @@ class ApplicationController < ActionController::Base
   def authenticate
     if (session = Session.find_by_id(cookies.signed[:session_token]))
       Current.session = session
-    # else
-    #   redirect_to sign_in_path
+    else
+      redirect_to sign_in_path
     end
   end
 
